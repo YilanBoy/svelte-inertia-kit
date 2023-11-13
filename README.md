57951d897b1bf113e517abc9459247817032bafd
allen-chiang <allen_chiang@trendmicro.com>
2023-11-13 18:05:05 +0800

    project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Inertia Starter-Kit
+
+This is a simple starter-kit for Laravel, Inertia, and Svelte.
---
+++ resources/js/Pages/Home.svelte
@@ -0,0 +1,9 @@
+<script>
+  let name = "allen";
+</script>
+
+<div>
+  <span class="text-2xl font-bold">
+    hello {name} !
+  </span>
+</div>
